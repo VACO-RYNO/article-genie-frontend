c9c596cf14bf1b6a61778b62115126685454cb68
changyun-go <aeiou75three@gmail.com>
2022-06-06 02:56:12 +0900

    🐛 token 존재 여부에 따라 header 요청이 되도록 수정

--- src/api/index.js
+++ src/api/index.js
@@ -1,37 +1,33 @@
 import axios from "axios";
 import config from "../config";
 
-let accessToken = "";
-
-if (localStorage.getItem("profile")) {
-  accessToken = JSON.parse(localStorage.getItem("profile")).token;
-}
-
 const API = axios.create({
   baseURL: config.SERVER_URL,
   timeout: 2500,
 });
 
-const axiosConfig = {
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
-  },
-};
+API.interceptors.request.use(req => {
+  if (localStorage.getItem("accessToken")) {
+    req.headers.Authorization = `Bearer ${JSON.parse(
+      localStorage.getItem("accessToken"),
+    )}`;
+  }
 
-export const login = () => API.post("/api/login", null, axiosConfig);
-export const getRecentSites = userId =>
-  API.get(`/api/users/${userId}/sites`, axiosConfig);
+  return req;
+});
+
+export const login = () => API.post("/api/login");
+export const getRecentSites = userId => API.get(`/api/users/${userId}/sites`);
 export const transformSite = originUrl => API.post("/api/sites", originUrl);
 export const getArticleList = userId =>
-  API.get(`/api/users/${userId}/articles`, axiosConfig);
+  API.get(`/api/users/${userId}/articles`);
 export const createArticle = (userId, article) =>
-  API.post(`/api/users/${userId}/articles`, article, axiosConfig);
+  API.post(`/api/users/${userId}/articles`, article);
 export const deleteArticle = (userId, articleId) =>
-  API.delete(`/api/users/${userId}articles/${articleId}`, null, axiosConfig);
+  API.delete(`/api/users/${userId}articles/${articleId}`);
 export const updateArticle = (userId, articleId, article) =>
-  API.put(`/api/users/${userId}articles/${articleId}`, article, axiosConfig);
+  API.put(`/api/users/${userId}articles/${articleId}`, article);
 export const updateLastVisitedSite = (userId, articleId, article) =>
-  API.patch(`/api/users/${userId}articles/${articleId}`, article, axiosConfig);
+  API.patch(`/api/users/${userId}articles/${articleId}`, article);
 export const getArticle = (userId, articleId) =>
-  API.get(`/api/users/${userId}articles/${articleId}`, axiosConfig);
+  API.get(`/api/users/${userId}articles/${articleId}`);
