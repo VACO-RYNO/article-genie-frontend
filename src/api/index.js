78b874fc716c977e1555efea14675391dd378333
junbro <khm11904@gmail.com>
2022-06-07 19:04:19 +0900

    ♻️ axios 모듈 수정

--- src/api/index.js
+++ src/api/index.js
@@ -17,17 +17,24 @@ API.interceptors.request.use(req => {
 });
 
 export const login = () => API.post("/api/login");
+
 export const getRecentSites = userId => API.get(`/api/users/${userId}/sites`);
-export const transformSite = originUrl => API.post("/api/sites", originUrl);
+export const transformSite = originUrl => API.post("/api/sites", { originUrl });
+
+export const createArticle = (userId, article) =>
+  API.post(`/api/users/${userId}/articles`, { ...article });
+
 export const getArticleList = userId =>
   API.get(`/api/users/${userId}/articles`);
-export const createArticle = (userId, article) =>
-  API.post(`/api/users/${userId}/articles`, article);
-export const deleteArticle = (userId, articleId) =>
-  API.delete(`/api/users/${userId}articles/${articleId}`);
-export const updateArticle = (userId, articleId, article) =>
-  API.put(`/api/users/${userId}articles/${articleId}`, article);
-export const updateLastVisitedSite = (userId, articleId, article) =>
-  API.patch(`/api/users/${userId}articles/${articleId}`, article);
 export const getArticle = (userId, articleId) =>
   API.get(`/api/users/${userId}articles/${articleId}`);
+
+export const updateArticle = (userId, articleId, article) =>
+  API.put(`/api/users/${userId}articles/${articleId}`, { ...article });
+export const updateLastVisitedSite = (userId, articleId, lastVisitedSiteUrl) =>
+  API.patch(`/api/users/${userId}articles/${articleId}`, {
+    lastVisitedSiteUrl,
+  });
+
+export const deleteArticle = (userId, articleId) =>
+  API.delete(`/api/users/${userId}articles/${articleId}`);
